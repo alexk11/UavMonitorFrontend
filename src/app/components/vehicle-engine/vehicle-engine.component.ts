9b422e1419b24c83e795cee783f2382c68a9ba4e
akryuchkov <akryuchkov1@gmail.com>
2025-12-22 17:50:51 +0300

    added engine component

--- src/app/components/vehicle-engine/vehicle-engine.component.ts
+++ src/app/components/vehicle-engine/vehicle-engine.component.ts
@@ -3,7 +3,6 @@ import {UavInfo} from "../../model/uav-info";
 import {UavEngine} from "../../model/uav-engine";
 import {HttpService} from "../../services/http.service";
 import {formatDate} from "@angular/common";
-import {AppConfigService} from "../../services/config.service";
 
 @Component({
   selector: 'app-vehicle-engine',
@@ -21,6 +20,9 @@ export class VehicleEngineComponent implements OnInit {
   date!: Date;
   progressbarInterval: number = 0
 
+  minutesTillNearestEngineTO: number = 0; //
+  timeTillNearestEngineTO: string = ''; // in format hh, min
+
   constructor(private httpService: HttpService) {}
 
   ngOnInit() {
@@ -34,8 +36,42 @@ export class VehicleEngineComponent implements OnInit {
       this.allRecords = data;
       data.forEach(val => this.shallow.push(Object.assign({}, val)));
       this.enumerateTOs();
+      this.timeTillNearestEngineTO = this.calculateTimeTillTO(data);
       //this.getProgressBarInterval();
     });
+  }
+
+  private calculateTimeTillTO(data: UavEngine[]): string {
+    let sum = 0;
+    data.forEach(val => {
+      sum += this.convertToDuration(val.engineOperateDuration);
+    })
+    if(sum <= 3000) {
+      this.minutesTillNearestEngineTO = 3000 - sum;
+      return this.toFormattedSpareDuration(3000 - sum);
+    } else if (3000 < sum && sum <= 6000) {
+      this.minutesTillNearestEngineTO = 6000 - sum;
+      return this.toFormattedSpareDuration(6000 - sum);
+    } else if (6000 < sum && sum <= 12000) {
+      this.minutesTillNearestEngineTO = 12000 - sum;
+      return this.toFormattedSpareDuration(12000 - sum);
+    } else {
+      const leftBound = Math.floor(sum / 6000);
+      const rightBound = (leftBound + 1) * 6000;
+      this.minutesTillNearestEngineTO = rightBound - sum;
+      return this.toFormattedSpareDuration(rightBound - sum);
+    }
+  }
+
+  private toFormattedSpareDuration(minutes: number): string {
+    const hh = Math.floor(minutes / 60);
+    const mm = minutes % 60;
+    return hh + ' ч, ' + (mm < 10 ? '0' + mm : mm) + ' мин';
+  }
+
+  private convertToDuration(durationStr: string): number {
+    const tokens = durationStr.split(' ');
+    return parseInt(tokens[0], 10)*60 + parseInt(tokens[2], 10);
   }
 
   // private getProgressBarInterval(): void {
@@ -73,7 +109,7 @@ export class VehicleEngineComponent implements OnInit {
     uavEngine.uavId = this.uav.uavId;
     uavEngine.engineOperateDuration = this.calculateInterval(uavEngine.engineActiveFrom, uavEngine.engineActiveTill);
     uavEngine.reporter = this.httpService.getUserName();
-    uavEngine.reportedTimestamp = formatDate(new Date(), 'dd.MM.yyyy HH:mm', 'en-US'); //this.toIsoDateString(formatDate(new Date(), 'yyyy-MM-dd HH:mm', 'en-US'));
+    uavEngine.reportedTimestamp = formatDate(new Date(), 'dd.MM.yy HH:mm', 'en-US'); //this.toIsoDateString(formatDate(new Date(), 'yyyy-MM-dd HH:mm', 'en-US'));
     this.httpService.saveUavTOInfo(uavEngine).subscribe((data: UavEngine) => {
       if (data !== null) {
         this.getTOData();
@@ -103,6 +139,26 @@ export class VehicleEngineComponent implements OnInit {
   private convertStringToDate(dateStr: string): Date {
     let d = dateStr.split(".");
     return new Date(d[2] + '/' + d[1] + '/' + d[0]);
+  }
+
+  getSeverityClass(): string {
+    if (this.minutesTillNearestEngineTO === undefined || this.minutesTillNearestEngineTO < 0) {
+      return 'status-warning';
+    }
+    if (this.minutesTillNearestEngineTO < 0) {
+      return 'status-expired';
+    } else if (this.minutesTillNearestEngineTO < 10) {
+      return 'status-critical';
+    } else if (this.minutesTillNearestEngineTO >= 10 && this.minutesTillNearestEngineTO < 30) {
+      return 'status-warning';
+    } else if (this.minutesTillNearestEngineTO >= 30 && this.minutesTillNearestEngineTO < 40) {
+      return 'status-attention';
+    }
+    return 'status-good';
+  }
+
+  getTimeLeftText(): string {
+    return 'До ближайшего ТО: ' + this.timeTillNearestEngineTO;
   }
 
   onRowEditInit(uavEngine: UavEngine) {

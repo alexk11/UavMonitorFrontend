dbff1edf691bb22cbb8e059c54f42c1def8abfe4
akryuchkov <akryuchkov1@gmail.com>
2026-01-13 17:05:46 +0300

    refactoring

--- src/app/services/config.service.ts
+++ src/app/services/config.service.ts
@@ -11,10 +11,13 @@ export interface AppConfig {
 @Injectable()
 export class AppConfigService {
 
+  private readonly backUrl: string = "http://192.168.18.151:8080/uav-montor-backend";
+  //private readonly backUrl: string = "http://localhost:8080";
+
   private config: AppConfig = {authToken: "", backUrl: "", userName: "", userRole: "", enabled: false};
 
   public initialize() {
-    this.config.backUrl = "http://localhost:8080";
+    this.config.backUrl = this.backUrl;
   }
 
   public getConfig(): AppConfig {
